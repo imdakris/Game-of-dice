75e627184e39668c01badc2e9cfed3c2bc68e194
Ruslan Dovhanych <imdakris@gmail.com>
2024-01-23 09:26:38 +0200

    app works but doesn't show icon

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from tkinter import *
 
 root = Tk()
-root.geometry('400x200')
-root.title('Game of Dice')
-
-
-
-
+root.geometry("400x200")
+root.title("Game of Dice")
+root.resizable(height=False, width=False)
+root.iconphoto(False, PhotoImage(file="iconka.png"))
 
 root.mainloop()
